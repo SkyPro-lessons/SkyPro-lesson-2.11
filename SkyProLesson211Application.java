9edb33a2543ffcd4542b6e343ab1016b90fe4573
Telion <www.neon@mail.ru>
2022-10-15 11:36:20 +0500

    Initial commit

---
+++ SkyProLesson211Application.java
@@ -0,0 +1,13 @@
+package site.telion.skyprolesson211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProLesson211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProLesson211Application.class, args);
+    }
+
+}
